0fcfabb400924e4118fbff3b127d5fee4ef3af00
Christy O'Reilly <christy@oreills.co.uk>
2012-03-18 20:58:52 +0000

    Chat server

--- src/csgame/core.cljs
+++ src/csgame/core.cljs
@@ -4,6 +4,12 @@
 (defn ^{:export greet} greet [n] 
   (str "Hello " n))
 
-(def ^{:export conn} conn (js/WebSocket.constructor "ws://localhost:8888"))
+(def ^{:export conn} conn (new js/WebSocket "ws://localhost:8888"))
+
+(aset conn "onopen" #(. conn send (str "Bot" (rand-int 100))))
+
+(aset conn "onerror" (fn [err] (. js/console log (str "WebSocket Error: " err))))
+
+(aset conn "onmessage" (fn [msg] (. js/console log (str "Server: " (aget msg "data")))))
 
 (repl/connect "http://localhost:9000/repl")
